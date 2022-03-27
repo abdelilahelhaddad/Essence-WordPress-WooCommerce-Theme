b3cb311491a25a4736e238038afa310d4a18ea4e
Abdelilah EL Haddad <elhaddadabdelilah@gmail.com>
2022-03-27 20:21:37 +0000

    :fire: :art: Add shop wraper using hooks + Remove sidebar :sparkles: :globe_with_meridians:

--- inc/woocommerce.php
+++ inc/woocommerce.php
@@ -8,43 +8,23 @@
  * @package essence
  */
 
-
-if (!function_exists('essence_woocommerce_wrapper_before')) {
-	/**
-	 * Before Content.
-	 *
-	 * Wraps all WooCommerce content in wrappers which match the theme markup.
-	 *
-	 * @return void
-	 */
-	function essence_woocommerce_wrapper_before()
-	{
+/** 
+ * Modify WooCommerce opening and closing HTML tags
+ * We need Bootstrap-like opening/closing HTML tags
+ */
+add_action('woocommerce_before_main_content', 'essence_open_container_row', 5);
+function essence_open_container_row()
+{
 ?>
-		<div class="container">
-			<div class="row">
-				<div class="col-12 col-sm-6 col-md-9 order-1 order-md-2">
-				<?php
-			}
-		}
-		add_action('woocommerce_before_main_content', 'essence_woocommerce_wrapper_before');
-
-		if (!function_exists('essence_woocommerce_wrapper_after')) {
-			/**
-			 * After Content.
-			 *
-			 * Closes the wrapping divs.
-			 *
-			 * @return void
-			 */
-			function essence_woocommerce_wrapper_after()
-			{
-				?>
-				</div>
-			</div>
+	<div class="container shop-content">
+		<div class="row">
+		<?php
+	}
+	add_action('woocommerce_after_main_content', 'essence_close_container_row', 5);
+	function essence_close_container_row()
+	{
+		?>
 		</div>
+	</div>
 <?php
-			}
-		}
-		add_action('woocommerce_after_main_content', 'essence_woocommerce_wrapper_after');
-
-		remove_action('woocommerce_sidebar', 'woocommerce_get_sidebar');
+	}
