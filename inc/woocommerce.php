d787f0cbee71894c934a077f1d8924c09a52d45e
Abdelilah EL Haddad <elhaddadabdelilah@gmail.com>
2022-03-27 20:23:43 +0000

    :fire: :art: Add shop wraper using hooks + Remove sidebar :sparkles: :globe_with_meridians:

--- inc/woocommerce.php
+++ inc/woocommerce.php
@@ -26,5 +26,33 @@ function essence_open_container_row()
 		?>
 		</div>
 	</div>
+	<?php
+	}
+
+	/** 
+	 * Remove the main WC sidebar from its original position
+	 * We'll be including it somewhere else later on
+	 */
+	remove_action('woocommerce_sidebar', 'woocommerce_get_sidebar');
+
+	// WooCommerce main sidebar will display only on Shop pages
+	if (is_shop()) {
+
+		add_action('woocommerce_before_main_content', 'essence_add_sidebar_tags', 6);
+		function essence_add_sidebar_tags()
+		{
+	?>
+		<div class="sidebar-shop col-lg-3 col-md-4 order-2 order-md-1">
+		<?php
+		}
+		// Put the main WC sidebar back, but using other action hook and on a different position
+		add_action('woocommerce_before_main_content', 'woocommerce_get_sidebar', 7);
+
+		add_action('woocommerce_before_main_content', 'essence_close_sidebar_tags', 8);
+		function essence_close_sidebar_tags()
+		{
+		?>
+		</div>
 <?php
+		}
 	}
